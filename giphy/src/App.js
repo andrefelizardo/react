090a10851f7f6011502cad12f4c3f37271b65037
Andre Felizardo <andreluis.felizardo@outlook.com>
2017-09-07 01:06:18 -0300

    Texto da tela inicial do app

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -4,13 +4,27 @@ import './libs/bootstrap.min.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand text-uppercase" href="#">Giphy!</a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#busca">Busca</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="jumbotron ">
+          <h1 className="display-3 text-uppercase">Giphy!</h1>
+          <p className="lead">Giphy foi uma das APIs que eu encontrei que são simples de trabalhar, então vamos brincar com ela.</p>
+          <hr className="my-4" />
+          <p>Se quiser ver toda a documentação da API, use o botão abaixo.</p>
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="https://developers.giphy.com/docs/#getting-started" role="button">Documentação API</a>
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
